45035656c1171cfe63ebc9e8ef839b91261140b0
unknown <adeshgupta101@gmail.com>
2024-06-07 23:17:03 +0530

    Database changes

--- config/db.sql
+++ config/db.sql
@@ -2,29 +2,34 @@ CREATE DATABASE library_management;
 USE library_management;
 
 CREATE TABLE users (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     username VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     role ENUM('client', 'admin') NOT NULL,
-    salt VARCHAR(255)
+    salt VARCHAR(255),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE books (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
     author VARCHAR(255) NOT NULL,
     isbn VARCHAR(13) NOT NULL,
     publication_year INT NOT NULL,
-    available BOOLEAN NOT NULL
+    total_copies INT UNSIGNED NOT NULL,
+    available_copies INT UNSIGNED NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE transactions (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    book_id INT NOT NULL,
-    transaction_datetime DATETIME NOT NULL,
-    is_accepted BOOLEAN NOT NULL DEFAULT FALSE,
+    transaction_id INT NOT NULL AUTO_INCREMENT,
+    user_id INT,
+    book_id INT,
+    status ENUM('checkout_requested', 'checkout_accepted', 'checkout_rejected', 'checkin_rejected', 'checkin_requested', 'returned') NOT NULL,
+    checkout_time DATETIME,
+    checkin_time DATETIME,
+    PRIMARY KEY (transaction_id),
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (book_id) REFERENCES books(id)
 );
